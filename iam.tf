6d85828e6e94d4144a17e50f762b2e0b8705fb09
TAPESHWAR RAY <115019353+tapeshwarray@users.noreply.github.com>
2024-07-30 15:52:34 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -60,4 +60,3 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_nodes.name
 }
-
