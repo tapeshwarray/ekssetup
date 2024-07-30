71d637626bb7057161a547e854eaec27000f293c
TAPESHWAR RAY <115019353+tapeshwarray@users.noreply.github.com>
2024-07-30 15:54:39 +0530

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,7 +10,11 @@ output "security_group_id" {
   value = aws_security_group.sg.id
 }
 
-#output "kubeconfig" {
-#  value = aws_eks_cluster.my_cluster.kubeconfig
-#}
+output "kubeconfig" {
+  value = kubernetes_namespace.example.id
+}
 
+# Output the EKS Cluster Endpoint using the lookup function
+output "eks_cluster_endpoint" {
+  value = lookup(aws_eks_cluster.aws_eks, "endpoint", "No endpoint found")
+}
